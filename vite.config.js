f7371a40cd1f17c5d6c388d52486e2a8d5f0fb82
wolf-yuan <wolf@wolf-yuan.dev>
2025-04-27 23:48:19 +0800

    feat: add sharp and svgo optimization to vite

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ViteImageOptimizer()],
   build: {
     cssMinify: "lightningcss",
   },
