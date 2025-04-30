77a1cc45b4f96b600fc9e5e26d8d3c5e57a88dcf
wolf-yuan <wolf@wolf-yuan.dev>
2025-04-30 21:35:03 +0800

    refactor: lower case looks better here :D

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import AstroIcon from "astro-icon";
+import astroIcon from "astro-icon";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
@@ -7,5 +7,5 @@ export default defineConfig({
   site: "https://sitcon.camp",
   base: "/2025",
   output: "static",
-  integrations: [AstroIcon()],
+  integrations: [astroIcon()],
 });
