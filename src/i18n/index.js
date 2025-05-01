08830596d9bce608f433f835f03553a8ef2a0985
wolf-yuan <wolf@wolf-yuan.dev>
2025-05-01 18:39:10 +0800

    feat: link transformer for translated page

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,4 +1,5 @@
 import { getLocale as extractLocale } from "astro-i18n-aut";
+import { posix } from "node:path";
 import enStrings from "./en/strings.js";
 import zhStrings from "./zh/strings.js";
 
@@ -9,4 +10,16 @@ export const rawStrings = {
 
 export const getLocale = (url) => {
   return rawStrings[extractLocale(url)];
+};
+
+export const linkTransformer = (url) => {
+  const currentLocale = extractLocale(url);
+
+  return (path, reverseLocale = false) => {
+    return (
+      (currentLocale === "en") !== reverseLocale
+        ? posix.join("/2025", "/en", path)
+        : posix.join("/2025", path)
+    ).replace(/\/+$/, "");
+  };
 };
