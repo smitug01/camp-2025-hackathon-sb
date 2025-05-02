5854ddded8f8dcd11ad838be0e1cc794a53df999
Elvis Mao <info@elvismao.com>
2025-05-02 11:10:22 +0800

    Reactor: EVERTHING (WIP)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 import "https://font.emtech.cc/emfont.js";
 
 emfont.init();
+
+const ad = () =>
+  console.log(
+    "%c既然你看到這裡了，那趕快去報名 SITCON Camp！",
+    "font-size: 40px; background-color: #009DF3; color: #FFF;font-family:system-ui;",
+  );
+
+ad();
+
+document.addEventListener("keydown", (e) => {
+  if (
+    // Ctrl+Shift+I (Windows/Linux) or Cmd+Option+I (Mac)
+    (e.ctrlKey && e.shiftKey && e.key === "I") ||
+    // Ctrl+Shift+J (Windows/Linux) or Cmd+Option+J (Mac)
+    (e.ctrlKey && e.shiftKey && e.key === "J") ||
+    e.key === "F12"
+  )
+    ad();
+});
