eab1ce58d61a7125868c58803fc49579be3af624
Elvis Mao <info@elvismao.com>
2025-06-22 14:43:27 -0500

    feat: 更新成員

---
+++ src/assets/data/teams.js
@@ -0,0 +1,126 @@
+export default [
+    {
+      name: "總召組",
+      members: [
+        { name: "康喔", title: "總召", emailHash: "018e4b6c4242d321d467d3294b30c240" },
+        { name: "柴柴", title: "副召", emailHash: "b1a99080023604ad99854985a48807ad" }
+      ]
+    },
+    {
+      name: "行政組",
+      members: [
+        { name: "Windless", title: "組長", emailHash: "25dd3f4cb755b23c48639be97ef54541" },
+        { name: "柴柴", title: "票務", emailHash: "b1a99080023604ad99854985a48807ad" }
+      ]
+    },
+    {
+      name: "財務組",
+      members: [
+        { name: "Arnoldsky", title: "組長", emailHash: "98688144e5d35e16ecdbcadd7d0880dd" },
+        { name: "Leaf", title: "出納", emailHash: "602818418612b2ea7b2ea810cc60a900" },
+        { name: "阿公", title: "組員", emailHash: "fa4ca3641aec4c08de52e1e3aec9ed24" }
+      ]
+    },
+    {
+      name: "課活組",
+      members: [
+        { name: "Nelson", title: "組長", emailHash: "f71508f754ae9659d6085846e023fdf2" },
+        { name: "Ricky", title: "副組長", emailHash: "d9d4323eaa5d051bb3cc75f5a8a82252" },
+        { name: "OsGa", title: "組員", emailHash: "4d9d9aa7e031eba958fd9ae26f9dacee" },
+        { name: "海鷗", title: "組員", emailHash: "d7d915ce07dbf6a5968c6bdfa2b5f0e2" },
+        { name: "Yuki", title: "組員", emailHash: "fbf80edc9ed4b30956a6532006dd8166" },
+        { name: "Panda", title: "組員", emailHash: "24c803aa3a79c7e3de16c21efe1d8791" },
+        { name: "Yoru", title: "組員", emailHash: "227841fd9810d2b702d10682eaa547ad" },
+        { name: "Kevin", title: "組員", emailHash: "b73e6c282e1187795151d6cc9bdefa93" },
+        { name: "EHDW", title: "組員", emailHash: "a08c0452802862375c33c9c75692c92f" },
+        { name: "亨", title: "組員", emailHash: "2ed2efd3ed0f399eb3bf2073241b61a9" }
+      ]
+    },
+    {
+      name: "庶務組",
+      members: [
+        { name: "Fearnot", title: "組長", emailHash: "582da575ff5a78f8f1271e3537f8b987" },
+        { name: "皮蛋", title: "副組長", emailHash: "973e7ddc08d7e07ce113d08ef3975ebd" },
+        { name: "yimang", title: "組員", emailHash: "7608dd727148eac7e8f6d71bcb689de3" },
+        { name: "Lindy", title: "組員", emailHash: "087b15325c4c1492de163636aae176c1" }
+      ]
+    },
+    {
+      name: "紀錄組",
+      members: [
+        { name: "OnCloud", title: "組長", emailHash: "993e1e8732a1039c2d1649fe953d6b0e" },
+        { name: "竺原", title: "組員", emailHash: "79bf1fd063a37be09ebc5a3560145cfd" },
+        { name: "pU", title: "組員", emailHash: "c4689697901d5f5d0ce2f49d2caa0fa4" },
+        { name: "柴柴", title: "組員", emailHash: "b1a99080023604ad99854985a48807ad" },
+        { name: "康喔", title: "組員", emailHash: "018e4b6c4242d321d467d3294b30c240" }
+      ]
+    },
+    {
+      name: "隊輔組",
+      members: [
+        { name: "OsGa", title: "組長", emailHash: "4d9d9aa7e031eba958fd9ae26f9dacee" },
+        { name: "咪路", title: "副組長", emailHash: "0d3eba022a4e0eda175441d9cf1e9061" },
+        { name: "小婕", title: "組員", emailHash: "2239772629220355486977364003cff3" },
+        { name: "諾諾", title: "組員", emailHash: "3994f6a8fef508df2fc4acdc7931557b" },
+        { name: "滷味", title: "組員", emailHash: "7319f02d14618ce536c4b8c62542f027" },
+        { name: "77", title: "組員", emailHash: "a65580fddf03a5a487dbba8f16e7b7f7" },
+        { name: "ffting", title: "組員", emailHash: "5471d3b548353d5c35fc873fb2422e3f" },
+        { name: "好冷吶", title: "組員", emailHash: "88f80e840a74e4bd10a12c375e4b38c3" },
+        { name: "萊姆", title: "組員", emailHash: "e6548ad6de87fbfe755ac88dff6c4829" },
+        { name: "阿玉", title: "組員", emailHash: "749cfae0b8ee5e52ed48909953de5df5" },
+        { name: "末夜", title: "組員", emailHash: "709d2e60f010f516f3f803cc627b59fa" },
+        { name: "Ak", title: "組員", emailHash: "5612d50fc4bb13fef29878c101b5d6fc" },
+        { name: "蓋蓋", title: "組員", emailHash: "4d0a2e7961d9ff36a2bb04c7036d84c2" },
+        { name: "小徐", title: "組員", emailHash: "1a89d85b1504bf9268d9cb0dec09e222" },
+        { name: "xiung", title: "組員", emailHash: "2eb851908601ac2ef8329010c5fea2e1" },
+        { name: "Yuto", title: "組員", emailHash: "b4bcc79d6599233491488d3a822bf3c1" },
+        { name: "Leo", title: "組員", emailHash: "9d3d94670521277a654d0096e8867fc5" },
+        { name: "Sam", title: "組員", emailHash: "abc5aa47604ff78d8d608f79f2b3c9b3" },
+        { name: "Wolf Yuan", title: "組員", emailHash: "dd35081a31561e03cd133d272f4faf30" },
+        { name: "Ya", title: "組員", emailHash: "69986fbca5d2211a55683b3876c14339" }
+      ]
+    },
+    {
+      name: "編輯組",
+      members: [
+        { name: "橘子", title: "組長", emailHash: "beee817f2bd8c2b195c243b2c50e6cc8" },
+        { name: "阿六", title: "副組長", emailHash: "91f69a45568cea093e4d8b23d6db7355" },
+        { name: "Yuan", title: "組員", emailHash: "58c71456209b3f04bffd2f88b9127ce0" },
+        { name: "13", title: "組員", emailHash: "ec736a2e7e5a43183f8eecf6deab5dfa" },
+        { name: "宸", title: "組員", emailHash: "80acb3ea7dcc9a32145993134df1a6a7" },
+        { name: "Mina", title: "組員", emailHash: "4275e3a6ffc16c94508d0ccbe9c3ce42" },
+        { name: "庭", title: "組員", emailHash: "3b89443b5dd5d850d857fda3ae69630b" },
+        { name: "Ya", title: "組員", emailHash: "69986fbca5d2211a55683b3876c14339" },
+         ]
+    },
+    {
+      name: "行銷組",
+      members: [
+        { name: "Denny Huang", title: "組長", emailHash: "9c08215f31eb6005a25be6521bf47b0a" },
+        { name: "康喔", title: "組員", emailHash: "018e4b6c4242d321d467d3294b30c240" },
+      ]
+    },
+    {
+      name: "資訊組",
+      members: [
+        { name: "毛哥EM", title: "組長", emailHash: "b661697559d0db601e1295dfaf0f0727" },
+        { name: "Wolf Yuan", title: "組員", emailHash: "dd35081a31561e03cd133d272f4faf30" },
+        { name: "KoukeNeko", title: "組員", emailHash: "6424b20d3c81c65c5597d41d263ddd7b" },
+        { name: "!Tony2100", title: "組員", emailHash: "dc799568cb03bceb24ab9c6774bc1a30" }
+      ]
+    },
+    {
+      name: "設計組",
+      members: [
+        { name: "兆翌", title: "組長", emailHash: "cdd8219acd6a05b986eed67c7dde5d0a" },
+        { name: "雞心", title: "組員", emailHash: "61d2fbcd7239b0c95452929fb9d09fa0" },
+        { name: "Henry", title: "組員", emailHash: "32d9ef56310ec50621b95275975f1bbc" },
+        { name: "花椰", title: "組員", emailHash: "4e11136653e9aad7eae399829d4dd4de" },
+        { name: "開根號", title: "組員", emailHash: "29e3bed56c56a577e89059c7c6adc6bd" },
+        { name: "章魚", title: "組員", emailHash: "f9070c1ecdbf423798132be0d99209fd" },
+        { name: "秋刀魚", title: "組員", emailHash: "fb136b6a2819f960c516d1952b791898" },
+        { name: "kiki", title: "組員", emailHash: "f538a69ee78a76546ecc118df295e944" },
+        { name: "毛哥EM", title: "組員", emailHash: "b661697559d0db601e1295dfaf0f0727" }
+      ]
+    }
+]
